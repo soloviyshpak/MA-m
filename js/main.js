63c16426172dbc431e915b123201254de2b3ba53
Artyom Soloveyov <soloviy.shpak16@gmail.com>
2024-03-18 19:03:33 +0300

    Added contacts.html

--- js/main.js
+++ js/main.js
@@ -62,3 +62,20 @@ $('.progress__slide-next').on('click', function (e) {
     e.preventDefault();
     $('.progress__slider').slick('slickNext');
 })
+
+// Map
+$('.contacts__map-btn--moscow').on('click', function (e) {
+    e.preventDefault();
+    $('#contacts__map-window').attr('src', 'https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d465979.11085226!2d37.61660764985341!3d55.77222625223984!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sru!4v1710776938638!5m2!1sru!2sru');
+    $('.contacts__map-btn').removeClass('contacts__map-btn--active');
+    $(this).addClass('contacts__map-btn--active');
+    $('.contacts__map-adress-item--adress').text('1-й Красногвардейский пр-д, д.1, к. 28,стр 2');
+    
+})
+$('.contacts__map-btn--piter').on('click', function (e) {
+    e.preventDefault();
+    $('#contacts__map-window').attr('src', 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d743191.8058471074!2d30.461823374472804!3d59.973781875640235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sru!4v1710773204048!5m2!1sru!2sru');
+    $('.contacts__map-btn').removeClass('contacts__map-btn--active');
+    $(this).addClass('contacts__map-btn--active');
+    $('.contacts__map-adress-item--adress').text('Покровская площадь, д.3, к. 5,стр 1');
+})
